8d44df3ddf8be86564ac7caa8999e7e771e54894
K M Rifat ul alom <kmrifat@gmail.com>
2022-07-16 14:26:40 +0600

    working on dynamic permission genarator

--- database/seeders/RoleAndPermissionsSeeder.php
+++ database/seeders/RoleAndPermissionsSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use Spatie\Permission\Models\Permission;
 
 class RoleAndPermissionsSeeder extends Seeder
@@ -22,38 +23,38 @@ class RoleAndPermissionsSeeder extends Seeder
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
         $super_admin = Role::create(['name' => 'Super Admin']);
 
-        $permissions = collect([
-            ['name' => 'dashboard', 'icon' => 'fas fa-columns', 'url' => '/dashboard', 'label' => 'Dashboard', 'description' => 'Dashboard', 'sidebar_menu' => 1, 'guard_name' => "sanctum"],
-        ]);
+        $permissions = collect();
+        $permissions->push($this->generatePermission('dashboard', []));
 
-//        $permissions->push(...$this->generatePermissionCRUD('users', '/user', 'User'));
+        $permissions->push(...$this->generatePermissionCRUD('user', ['url' => '/user']));
 
-
-//        echo $permissions;
-//        dd(...$permissions);
-        dd($this->generatePermission('Dashboard', 'url'));
+        dd($permissions);
 
     }
 
-    private function generatePermission($name, $options)
+    private function generatePermission($name, $args = [])
     {
-        return $options;
-//        if ($optional['label'] == null) $optional['label'] = $name;
-//        if ($optional['description'] == null) $optional['description'] = $name;
-//
-//        return collect([
-//            ['name' => $name, 'icon' => $optional['icon'], 'url' => $optional['url'], 'label' => $optional['label'], 'description' => $optional['description'], 'sidebar_menu' => $optional['sidebar_menu'], 'guard_name' => "sanctum"],
-//        ]);
+        return [
+            'name' => $name,
+            'icon' => array_key_exists('icon', func_get_args()[1]) ? func_get_args()[1]['icon'] : 'fas fa-bars',
+            'url' => array_key_exists('url', func_get_args()[1]) ? func_get_args()[1]['url'] : "/" . Str::slug($name),
+            'sidebar_menu' => array_key_exists('sidebar_menu', func_get_args()[1]) ? func_get_args()[1]['sidebar_menu'] : 0,
+            'guard_name' => array_key_exists('guard_name', func_get_args()[1]) ? func_get_args()[1]['guard_name'] : 'sanctum',
+            'description' => array_key_exists('description', func_get_args()[1]) ? func_get_args()[1]['description'] : '',
+            'label' => array_key_exists('label', func_get_args()[1]) ? func_get_args()[1]['label'] : Str::title($name),
+        ];
     }
 
-
-    private function generatePermissionCRUD($table, $url, $menu_label, $icon = 'fa-solid fa-bar'): Collection
+    private function generatePermissionCRUD($name, $args = [])
     {
+        $icon = array_key_exists('icon', $args) ? $args['icon'] : 'fas fa-bars';
+        $url = array_key_exists('url', $args) ? $args['url'] : "/" . Str::slug($name);
+        $label = array_key_exists('label', $args) ? $args['label'] : Str::title($name);
         return collect([
-            ['name' => "create {$table}", 'description' => "Can create {$table}", "icon" => $icon, "url" => null, "label" => null, "sidebar_menu" => 0, "guard_name" => "sanctum"],
-            ['name' => "update {$table}", 'description' => "Can create {$table}", "icon" => $icon, "url" => null, "label" => null, "sidebar_menu" => 0, "guard_name" => "sanctum"],
-            ['name' => "show {$table}", 'description' => "Can create {$table}", "icon" => $icon, 'url' => "{$url}", 'label' => "{$menu_label}", 'sidebar_menu' => 1, "guard_name" => "sanctum"],
-            ['name' => "delete {$table}", 'description' => "Can create {$table}", "icon" => $icon, "url" => null, "label" => null, "sidebar_menu" => 0, "guard_name" => "sanctum"]
+            $this->generatePermission("create {$name}", ['description' => "Can create {$name}", 'icon' => $icon]),
+            $this->generatePermission("update {$name}", ['description' => "Can update {$name}", 'icon' => $icon]),
+            $this->generatePermission("show {$name}", ['description' => "Can show {$name}", 'sidebar_menu' => 1, 'icon' => $icon, 'url' => $url, 'label' => $label]),
+            $this->generatePermission("delete {$name}", ['description' => "Can delete {$name}", 'icon' => $icon]),
         ]);
     }
 }
